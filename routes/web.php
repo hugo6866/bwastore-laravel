e01b4d7c6b38493d4c066464807da8dec1de32ef
Ciev <hafizhhugo@gmail.com>
2023-03-23 15:42:00 +0700

    create and save users

--- routes/web.php
+++ routes/web.php
@@ -38,6 +38,7 @@ Route::prefix('admin')
     ->group(function () {
         Route::get('/', [App\Http\Controllers\Admin\DashboardController::class, 'index'])->name('admin-dashboard');
         Route::resource('category', "CategoryController");
+        Route::resource('user', "UserController");
     });
 
 Auth::routes();
