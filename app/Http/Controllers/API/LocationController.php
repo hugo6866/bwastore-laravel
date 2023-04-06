f0f3c34a1fb64c8f974b2c7a92f6ca9b2289f0a8
Ciev <hafizhhugo@gmail.com>
2023-04-06 22:08:29 +0700

    fix api moment

--- app/Http/Controllers/API/LocationController.php
+++ app/Http/Controllers/API/LocationController.php
@@ -5,13 +5,14 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Province;
+use App\Models\Regency;
 class LocationController extends Controller
 {
     public function provinces(Request $request){
         return Province::all();
     }
     public function regencies(Request $request, $provices_id){
-        return Province::where('province_id', $provices_id->get());
+        return Regency::where('province_id', $provices_id)->get();
     }
     //
 }
--- resources/views/pages/cart.blade.php
+++ resources/views/pages/cart.blade.php
@@ -34,6 +34,7 @@
                                 </tr>
                             </thead>
                             <tbody>
+                                @php $totalPrice = 0 @endphp
                                 @foreach ($carts as $cart)
                                     @if ($cart->product)
                                         <tr>
@@ -60,6 +61,7 @@
                                                 </form>
                                             </td>
                                         </tr>
+                                        @php $totalPrice += $cart->product->price @endphp
                                     @endif
                                 @endforeach
                             </tbody>
@@ -74,88 +76,139 @@
                         <h2 class="mb-4">Shipping Details</h2>
                     </div>
                 </div>
-                <div class="row mb-2" data-aos="fade-up" data-aos-delay="200">
-                    <div class="col-md-6">
-                        <div class="form-group">
-                            <label for="addressOne">Address 1</label>
-                            <input type="text" class="form-control mt-2 mb-2" id="addressOne" name="addressOne"
-                                value="Setra Duta Cemara" />
+                <form action="">
+                    <div class="row mb-2" data-aos="fade-up" data-aos-delay="200" id="locations">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="address_one">Address 1</label>
+                                <input type="text" class="form-control mt-2 mb-2" id="address_one" name="address_one"
+                                    value="Setra Duta Cemara" />
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="address_two">Address 2</label>
+                                <input type="text" class="form-control mt-2 mb-2" id="address_two" name="address_two"
+                                    value="Blok B No. 34" />
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                <label for="provinces_id">Province</label>
+                                <select name="provinces_id" id="provinces_id" class="form-control mt-2 mb-2"
+                                    :v-if="provinces" v-model="provinces_id">
+                                    <option v-for="province in provinces" :value="provinces.id">@{{ province.name }}
+                                    </option>
+                                    <select v-else class="form-control"></select>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                <label for="regencies_id">City</label>
+                                <input type="text" class="form-control mt-2 mb-2" id="regencies_id" name="regencies_id"
+                                    value="Bandung" />
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                <label for="zip_code">Postal Code</label>
+                                <input type="text" class="form-control mt-2 mb-2" id="zip_code" name="zip_code"
+                                    value="53243" />
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="country">Country</label>
+                                <input type="text" class="form-control mt-2 mb-2" id="country" name="country"
+                                    value="Indonesia" />
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="phone_number">Mobile</label>
+                                <input type="text" class="form-control mt-2 mb-2" id="phone_number" name="phone_number"
+                                    value="+62 8932 4212 6323" />
+                            </div>
                         </div>
                     </div>
-                    <div class="col-md-6">
-                        <div class="form-group">
-                            <label for="addressTwo">Address 2</label>
-                            <input type="text" class="form-control mt-2 mb-2" id="addressTwo" name="addressTwo"
-                                value="Blok B No. 34" />
+                    <div class="row" data-aos="fade-up" data-aos-delay="150">
+                        <div class="col-12">
+                            <hr />
+                        </div>
+                        <div class="col-12">
+                            <h2 class="mb-2">Payment Informations</h2>
                         </div>
                     </div>
-                    <div class="col-md-4">
-                        <div class="form-group">
-                            <label for="province">Province</label>
-                            <select name="province" id="province" class="form-control mt-2 mb-2">
-                                <option value="West Java">West Java</option>
-                            </select>
+                    <div class="row mb-5">
+                        <div class="col-4 col-md-2">
+                            <div class="product-title">$10</div>
+                            <div class="product-subtitle">Country Tax</div>
+                        </div>
+                        <div class="col-4 col-md-3">
+                            <div class="product-title">$280</div>
+                            <div class="product-subtitle">Product Insurancce</div>
+                        </div>
+                        <div class="col-4 col-md-2">
+                            <div class="product-title">$500</div>
+                            <div class="product-subtitle">Ship to Jakarta</div>
+                        </div>
+                        <div class="col-4 col-md-2">
+                            <div class="product-title text-success">${{ number_format($totalPrice ?? 0) }}</div>
+                            <div class="product-subtitle">Total</div>
+                        </div>
+                        <div class="col-8 col-md-3">
+                            <a href="/success.html" class="btn btn-success mt-4 px-4 btn-block">Checkout Now</a>
                         </div>
                     </div>
-                    <div class="col-md-4">
-                        <div class="form-group">
-                            <label for="city">City</label>
-                            <input type="text" class="form-control mt-2 mb-2" id="city" name="city"
-                                value="Bandung" />
-                        </div>
-                    </div>
-                    <div class="col-md-4">
-                        <div class="form-group">
-                            <label for="postalcode">Postal Code</label>
-                            <input type="text" class="form-control mt-2 mb-2" id="postalcode" name="postalcode"
-                                value="53243" />
-                        </div>
-                    </div>
-                    <div class="col-md-6">
-                        <div class="form-group">
-                            <label for="country">Country</label>
-                            <input type="text" class="form-control mt-2 mb-2" id="country" name="country"
-                                value="Indonesia" />
-                        </div>
-                    </div>
-                    <div class="col-md-6">
-                        <div class="form-group">
-                            <label for="mobile">Mobile</label>
-                            <input type="text" class="form-control mt-2 mb-2" id="mobile" name="mobile"
-                                value="+62 8932 4212 6323" />
-                        </div>
-                    </div>
-                </div>
-                <div class="row" data-aos="fade-up" data-aos-delay="150">
-                    <div class="col-12">
-                        <hr />
-                    </div>
-                    <div class="col-12">
-                        <h2 class="mb-2">Payment Informations</h2>
-                    </div>
-                </div>
-                <div class="row" data-aos="fade-up" data-aos-delay="200">
-                    <div class="col-4 col-md-2">
-                        <div class="product-title">$10</div>
-                        <div class="product-subtitle">Country Tax</div>
-                    </div>
-                    <div class="col-4 col-md-3">
-                        <div class="product-title">$280</div>
-                        <div class="product-subtitle">Product Insurancce</div>
-                    </div>
-                    <div class="col-4 col-md-2">
-                        <div class="product-title">$500</div>
-                        <div class="product-subtitle">Ship to Jakarta</div>
-                    </div>
-                    <div class="col-4 col-md-2">
-                        <div class="product-title text-success">$392,409</div>
-                        <div class="product-subtitle">Total</div>
-                    </div>
-                    <div class="col-8 col-md-3">
-                        <a href="/success.html" class="btn btn-success mt-4 px-4 btn-block">Checkout Now</a>
-                    </div>
-                </div>
+                </form>
             </div>
         </section>
     </div>
 @endsection
+
+@push('addon-script')
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+    <script src="https://unpkg.com/axios@1.1.2/dist/axios.min.js"></script>
+
+    <script>
+        const app = Vue.createApp({
+            data() {
+                return {
+                    provinces: null,
+                    regencies: null,
+                    provinces_id: null,
+                    regencies_id: null,
+                };
+            },
+            methods: {
+                async getProvincesData() {
+                    try {
+                        const response = await axios.get('{{ route('api-provinces') }}');
+                        this.provinces = response.data;
+                    } catch (error) {
+                        console.error('Error fetching provinces:', error);
+                    }
+                },
+                getRegenciesData() {
+                    axios.get('{{ url('api/regencies') }}/' + 1)
+                        .then(function(response) {
+                            this.regencies = response.data;
+                        })
+                }
+            },
+            mounted() {
+                AOS.init();
+                this.getProvincesData();
+
+            },
+            watch: {
+                provinces_id: function(val, oldVal) {
+                    this.regencies_id = null;
+                    this.getRegenciesData();
+                }
+            }
+        });
+        app.mount("#locations");
+    </script>
+@endpush
